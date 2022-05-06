fd1360777fdf65d7331bf09b2213ca92e7694b02
seunghwan Choi <dexrf1@gmail.com>
2022-05-06 19:48:43 +0900

    initialize project

---
+++ src/main/java/com/system/wood/WoodApplication.java
@@ -0,0 +1,13 @@
+package com.system.wood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoodApplication.class, args);
+	}
+
+}
