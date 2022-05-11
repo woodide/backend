a6d59bb1bec497fc78082f78df086576d3517bb8
seunghwan Choi <dexrf1@gmail.com>
2022-05-12 01:36:53 +0900

    feat: entity class 필드 및 여러 제약사항 추가

--- src/main/java/com/system/wood/domain/Member.java
+++ src/main/java/com/system/wood/domain/Member.java
@@ -1,11 +1,14 @@
 package com.system.wood.domain;
 
-import com.system.wood.domain.Role;
+import com.sun.istack.NotNull;
+import com.system.wood.domain.container.Container;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor
 @Getter
@@ -13,18 +16,26 @@ import javax.persistence.*;
 public class Member {
 
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
 
+    @Column(unique = true)
     private String email;
 
+    @NotNull
     private String password;
 
+    @NotNull
     private String username;
 
     @Enumerated(EnumType.STRING)
     private Role role;
 
+    @OneToMany(mappedBy = "member")
+    private List<Container> containerList = new ArrayList<>();
+
     @Builder
+
     public Member(String email, String password, String username, Role role) {
         this.email = email;
         this.password = password;
--- src/main/java/com/system/wood/domain/container/Container.java
+++ src/main/java/com/system/wood/domain/container/Container.java
@@ -1,6 +1,8 @@
 package com.system.wood.domain.container;
 
+import com.sun.istack.NotNull;
 import com.system.wood.domain.Member;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -12,11 +14,31 @@ import javax.persistence.*;
 public class Container {
 
     @Id @GeneratedValue
+    @Column(name = "container_id")
     private Long id;
 
+    @Column(unique = true, nullable = false)
     private Integer portNum;
+
+    @Column(name = "docker_container_id", unique = true, nullable = false)
+    private String dockerContainerId;
+
+    @NotNull
+    private String containerName;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
+
+    @Builder
+    public Container(Integer portNum, String dockerContainerId, Member member, String containerName) {
+        this.portNum = portNum;
+        this.dockerContainerId = dockerContainerId;
+        this.containerName = containerName;
+        this.member = member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
 }
