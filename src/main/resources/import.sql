2dd11f3343d572b1a459b04e1a7e10c6be8f03e5
seunghwan Choi <dexrf1@gmail.com>
2022-05-31 03:28:02 +0900

    styles: gitignore 적용

--- src/main/resources/import.sql
+++
@@ -1,4 +0,0 @@
--- Member
-
-insert into member values(1000, 'dexrf1@gmail.com', '1234','PROFESSOR', 'shwan97')
-insert into member values(1000, 200, 'dexrf1@gmail.com', '1234','PROFESSOR', 'shwan97')
